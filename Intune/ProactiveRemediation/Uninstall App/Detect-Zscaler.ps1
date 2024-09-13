9d3ee1c2b1bb8a6982fd2810aefb41f667c5d74f
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-13 15:52:33 -0400

    Create Detect-Zscaler.ps1

---
+++ Intune/ProactiveRemediation/Uninstall App/Detect-Zscaler.ps1
@@ -0,0 +1,38 @@
+<#
+.NOTES
+    Filename: Detect-Zscaler.ps1
+    Version: 1.0
+    Author: Joel Cottrell
+    Modified by: Joel Cottrell
+    Requires: PowerShell v5
+    Version History:
+    1.0 - 24/08/15 - Initial release of this script.
+.SYNOPSIS
+    This script package will attempt to detect the Zscaler application on the targeted devices.
+    This script is used as detection script with Proactive Remediations in the Microsoft Intune admin center.
+.DESCRIPTION
+    Detect the Zscaler application.
+
+.EXAMPLE
+$appVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -like "Zscaler*" } | Select-Object -Property DisplayName, UninstallString	
+
+#>
+
+# Detection script
+$appdetails = (Get-ItemProperty Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*) | Where-Object {$_.DisplayName -eq "Zscaler"}
+try {
+    if ($appdetails.DisplayName -eq "Zscaler"){
+    Write-Output "Zscaler is installed and will now be removed"
+    Exit 1
+    }
+
+    else {
+    Write-Output "Zscaler is not installed. No action required"
+    Exit 0
+}
+}
+
+ catch{
+    $errMsg = $_.exeption.essage
+    Write-Output $errMsg
+ }
