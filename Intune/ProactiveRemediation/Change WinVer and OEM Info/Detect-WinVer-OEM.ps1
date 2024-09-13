df623352aafe01f77d1843c1a8dff0daf05c18a0
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-13 15:15:51 -0400

    Create Detect-WinVer-OEM.ps1

---
+++ Intune/ProactiveRemediation/Change WinVer and OEM Info/Detect-WinVer-OEM.ps1
@@ -0,0 +1,46 @@
+<#  
+.NOTES  
+    Name: Detect-WinVer-OEM.ps1  
+    Author: Niklas Rast
+    Modified by: Joel Cottrell  
+    Requires: PowerShell v5 
+    Version History:  
+    1.0 - 23/12/11 - Initial release of this script.  
+.SYNOPSIS  
+    Change the WinVer and OEM Info on a Windows device.
+.DESCRIPTION  
+    This Proactive Remediation detection script checks to see whether the WinVer and OEM Info
+    entries in the registry contain values.
+
+    Install:   C:\Windows\SysNative\WindowsPowershell\v1.0\PowerShell.exe -ExecutionPolicy Bypass -Command .\Detect-WinVer-OEM.ps1
+
+    Inspiration in creating this was provide by steps found in this link:
+    
+    https://niklasrast.com/2023/10/05/elevate-your-corporate-branding-on-managed-windows-devices-with-microsoft-intune-remediations/
+
+#>
+
+$BrandingContent = @"
+RegKeyPath,Key,Value
+"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation","SupportURL","https://advizex.service-now.com/"
+"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation","Manufacturer","Lenovo"
+"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation","SupportHours","Standard: 8AM - 5PM EST"
+"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation","SupportPhone","(617) 715-3774"
+"HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion","RegisteredOwner","IntelyCare"
+"HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion","RegisteredOrganization","IntelyCare"
+"@
+
+$Branding = $BrandingContent | ConvertFrom-Csv -delimiter ","
+
+foreach ($Brand in $Branding) {
+    $ExistingValue = (Get-Item -Path $($Brand.RegKeyPath)).GetValue($($Brand.Key))
+    if ($ExistingValue -ne $($Brand.Value)) {
+      Write-Host $($Brand.Key) "Not Equal"
+      Exit 1
+      Exit #Remediation 
+    }
+    else {
+#      Write-Host $($Brand.Key) "Equal"
+    }
+}
+Exit 0
