7f16cbb160f8c268b9d645688f75c2cd8414afe1
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-23 10:28:28 -0400

    Update Detect-Netskope.ps1

--- Intune/Compliance/Netskope/Detect-Netskope.ps1
+++ Intune/Compliance/Netskope/Detect-Netskope.ps1
@@ -1,67 +1,78 @@
-<#  
-.NOTES  
-    Name: Detect-Netskope.ps1  
-    Author: Joel Cottrell  
-    Requires: PowerShell v2 
-    Version History:  
-    2.0 - 24/09/13 - Initial release of this script.  
-.SYNOPSIS  
-    A custom Intune compliance detection script to detect if the Netskope client is running or not.
-.DESCRIPTION  
-    The custom compliance detection script (used in an Intune Compliance configuration)
-    checks whether the Netskope application/service is running and outputs a true or 
-    false JSON value.
+<#	
+.NOTES
+	Name: Detect-Netskope.ps1 
+	Author: Joel Cottrell
+	Copyright: GPLv3
+	Tags: intune endpoint MEM netskope
 
-    Inspiration in creating this was provide by steps found in this link:
+.LICENSEURI
+https://github.com/bigjoestretch/public/blob/main/LICENSE
+
+.PROJECTURI
+https://github.com/bigjoestretch/public/tree/main/Intune/Compliance/Netskope
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+v1.0 - 24/09/23 - Initial release of this script.
+	
+.SYNOPSIS
+Custom compliance script to report Netskope Client service state and startup configuration
+
+.DESCRIPTION
+This script checks the Netskope Client service Startup Type and Service state. The service must be 'running' and startup must be set to 'Automatic'
     
-    https://www.nielskok.tech/intune/zscaler-custom-compliance-in-intune/
-    https://jannikreinhard.com/2023/02/26/how-to-use-custom-compliance-script-example-script/
+.EXAMPLE
+Example output: {"ServiceState":4,"ServiceStartupMode":2}
 
-#> 
+#>
 
-# Function to check if the Netskope Client is running
-function Is-NetskopeRunning {
-    # Get all running processes
-    $processes = Get-Process
+$ServiceState = get-service -name "stAgentSvc" | Select-object Status
+$ServiceStartupMode = get-service -name "stAgentSvc" | Select-object StartType
 
-    # Check if any process name contains 'netskope'
-    foreach ($process in $processes) {
-        if ($process.Description -like "*netskope*") {
-            return $true
-        }
-    }
-    return $false
+$output = @{
+	ServiceState = $ServiceState.Status
+	ServiceStartupMode = $ServiceStartupMode.StartType
 }
 
-# Check if Netskope Private Access is running and print the result
-#if (Is-NetskopeRunning) {
-#    Write-Output "Netskope Private Access is running."
-#} else {
-#    Write-Output "Netskope Private Access is not running."
-#}
-
-# Detection script outputs the JSON value for the Netskope result
-$checknetskope = Is-NetskopeRunning
-$NetskopeStatus = @{"NetskopeStatus" = $checknetskope}
-
-return $NetskopeStatus | ConvertTo-Json -Compress
+return $output | ConvertTo-Json -Compress
 
 # This section below should be saved As a JSON file and added/imported to an Intune Custom Compliance policy):
 #{
 #    "Rules":[ 
 #        { 
-#           "SettingName":"$NetskopeStatus",
-#           "Operator":"NotEquals",
-#           "DataType":"String",
-#           "Operand":"false",
+#           "SettingName":"ServiceState",
+#           "Operator":"IsEquals",
+#           "DataType":"Int64",
+#           "Operand":4,
 #           "MoreInfoUrl":"https://docs.netskope.com/en/netskope-client-troubleshooting-guide/",
 #           "RemediationStrings":[ 
 #              { 
-#                 "Language":"en_US",
-#                 "Title":"The Netskope client is not running.",
-#                 "Description": "Your device must have the Netskope client running and enabled. Try restarting your device, and then follow the steps here: https://docs.netskope.com/en/netskope-client-troubleshooting-guide/. If you do this and get this message again, contact Corporate IT at itsupport@intelycare.com."
+#                 "Language": "en_US",
+#                 "Title": "The Netskope client service must be running. Value discovered was {ActualValue}.",
+#                 "Description": "Your device must have the Netskope client service running and enabled. Try restarting your device, and then follow the steps here: https://docs.netskope.com/en/netskope-client-troubleshooting-guide/. If you do this and get this message again, contact Corporate IT at itsupport@intelycare.com."
 #              }
 #           ]
-#        }
+#        },
+#        { 
+#         "SettingName":"ServiceStartupMode",
+#         "Operator":"IsEquals",
+#         "DataType":"Int64",
+#         "Operand":2,
+#         "MoreInfoUrl":"https://docs.netskope.com/en/netskope-client-troubleshooting-guide/",
+#         "RemediationStrings":[ 
+#            { 
+#               "Language": "en_US",
+#               "Title": "The Netskope client service startup must be set to Automatic. Value discovered was {ActualValue}.",
+#               "Description": "Your device must have the Netskope client service startup set to Automatic. Try restarting your device, and then follow the steps here: https://docs.netskope.com/en/netskope-client-troubleshooting-guide/. If you do this and get this message again, contact Corporate IT at itsupport@intelycare.com."
+#            }
+#         ]
+#      }
 #     ]
-#}
+#    }
