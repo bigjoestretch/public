c2dec43f0fc1520a52d9f12ebe9dde3db06f5d8d
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-25 15:54:30 -0400

    Add files via upload

---
+++ Intune/Win32Apps/Azul Zulu OpenJRE/install-zulu-jre.ps1
@@ -0,0 +1,42 @@
+<#	
+.NOTES
+	Name: install-zulu-jre.ps1
+	Author: Joel Cottrell
+	Copyright: GPLv3
+	Tags: intune endpoint MEM azul zulu jre
+
+.LICENSEURI
+https://github.com/bigjoestretch/public/blob/main/LICENSE
+
+.PROJECTURI
+https://github.com/bigjoestretch/public/tree/main/Intune/Compliance/Cisco%20Umbrella
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+v1.0 - 24/09/25 - Initial release of this script.
+	
+.SYNOPSIS
+Install script used to install the Azul Zulu Build of OpenJDK on Windows.
+
+.DESCRIPTION
+This script is used to install the Azul Zulu Build of OpenJDK onto a Windows device.
+
+Inspiration in creating this was provide by steps found in this link:
+
+https://docs.azul.com/core/install/windows
+    
+.EXAMPLE
+./install-zulu-jre.ps1
+
+#>
+
+#Silently install Azul Zulu JRE version 21.0.4
+
+msiexec /i zulu21.36.17-ca-jre21.0.4-win_x64.msi ADDLOCAL=ZuluInstallation,FeatureJavaHome INSTALLDIR="c:\java\jdk21" /qn
---
+++ Intune/Win32Apps/Azul Zulu OpenJRE/uninstall-zulu-jre.ps1
@@ -0,0 +1,42 @@
+<#	
+.NOTES
+	Name: uninstall-zulu-jre.ps1
+	Author: Joel Cottrell
+	Copyright: GPLv3
+	Tags: intune endpoint MEM azul zulu jre
+
+.LICENSEURI
+https://github.com/bigjoestretch/public/blob/main/LICENSE
+
+.PROJECTURI
+https://github.com/bigjoestretch/public/tree/main/Intune/Compliance/Cisco%20Umbrella
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+v1.0 - 24/09/25 - Initial release of this script.
+	
+.SYNOPSIS
+Uninstall script used to uninstall the Azul Zulu Build of OpenJDK on Windows.
+
+.DESCRIPTION
+This script is used to uninstall the Azul Zulu Build of OpenJDK from a Windows device.
+
+Inspiration in creating this was provide by steps found in this link:
+
+https://docs.azul.com/core/uninstall/windows
+    
+.EXAMPLE
+./uninstall-zulu-jre.ps1
+
+#>
+
+#Silently uninstall Azul Zulu JRE
+
+msiexec /x (Get-Package 'Azul Zulu JRE*').FastPackageReference /qn
