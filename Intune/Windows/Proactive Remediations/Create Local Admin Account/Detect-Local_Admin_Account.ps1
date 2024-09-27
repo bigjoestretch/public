0a354dc24c48b0c795f1229092c2a5d420442b0c
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-27 12:28:03 -0400

    Update and rename Intune/Proactive Remediations/Create Local Admin Account/Detect-Local_Admin_Account.ps1 to Intune/Windows/Proactive Remediations/Create Local Admin Account/Detect-Local_Admin_Account.ps1

--- Intune/Windows/Proactive Remediations/Create Local Admin Account/Detect-Local_Admin_Account.ps1
+++ Intune/Windows/Proactive Remediations/Create Local Admin Account/Detect-Local_Admin_Account.ps1
@@ -3,14 +3,35 @@
     Filename: Detect-Local_Admin_Account.ps1
     Version: 1.0
     Author: Joel Cottrell
-    Version History:
-    1.0 - 23/05/23 - Initial release of this script.
+    Requires: PowerShell v5 
+    Copyright: GPLv3
+	Tags: intune endpoint MEM local admin
+ 
+.LICENSEURI
+https://github.com/bigjoestretch/public/blob/main/LICENSE
+
+.PROJECTURI
+https://github.com/bigjoestretch/public/blob/main/Intune/Windows/Proactive%20Remediations
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+v1.0 - 23/05/23 - Initial release of this script.
+
 .SYNOPSIS
     This script package will attempt to detect a local administrator account on the targeted devices.
     This script is used as detection script with Proactive Remediations in the Microsoft Intune admin center.
+
 .DESCRIPTION
     Manage a local administrator account in Windows, which currently consists of:
     - XXXX-XXXXX
+
 .LINK
     Inspiration in creating this was provide y steps found in this link:
     
