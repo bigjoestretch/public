1ee89d5e040bda21493a7b92b3147b0fdae4a8bf
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-27 12:31:27 -0400

    Update and rename Intune/Proactive Remediations/Uninstall App/Zscaler/Detect-Zscaler.ps1 to Intune/Windows/Proactive Remediations/Uninstall App/Zscaler/Detect-Zscaler.ps1

--- Intune/Windows/Proactive Remediations/Uninstall App/Zscaler/Detect-Zscaler.ps1
+++ Intune/Windows/Proactive Remediations/Uninstall App/Zscaler/Detect-Zscaler.ps1
@@ -5,8 +5,27 @@
     Author: Joel Cottrell
     Modified by: Joel Cottrell
     Requires: PowerShell v5
-    Version History:
-    1.0 - 24/08/15 - Initial release of this script.
+    Requires: PowerShell v5 
+    Copyright: GPLv3
+	Tags: intune endpoint MEM zscaler
+ 
+.LICENSEURI
+https://github.com/bigjoestretch/public/blob/main/LICENSE
+
+.PROJECTURI
+https://github.com/bigjoestretch/public/blob/main/Intune/Proactive%20Remediations/Uninstall%20App/Zscaler
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+v1.0 - 24/08/15 - Initial release of this script.
+
 .SYNOPSIS
     This script package will attempt to detect the Zscaler application on the targeted devices.
     This script is used as detection script with Proactive Remediations in the Microsoft Intune admin center.
