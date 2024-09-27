ea5af7497cdec3732b0cf3ca08c2ad5f9140ebbc
Joel Cottrell <47227068+bigjoestretch@users.noreply.github.com>
2024-09-27 12:10:08 -0400

    Update and rename Intune/Compliance/Cisco Umbrella/Detect-Cisco-Umbrella.ps1 to Intune/Windows/Compliance/Cisco Umbrella/Detect-Cisco-Umbrella.ps1

--- Intune/Windows/Compliance/Cisco Umbrella/Detect-Cisco-Umbrella.ps1
+++ Intune/Windows/Compliance/Cisco Umbrella/Detect-Cisco-Umbrella.ps1
@@ -9,7 +9,7 @@
 https://github.com/bigjoestretch/public/blob/main/LICENSE
 
 .PROJECTURI
-https://github.com/bigjoestretch/public/tree/main/Intune/Compliance/Cisco%20Umbrella
+https://github.com/bigjoestretch/public/blob/main/Intune/Windows/Compliance/Cisco%20Umbrella
 
 .ICONURI
 
